0216184532ec18698e715bed874f9b5eb2044aeb
ycui1 <yong.cui01@gmail.com>
2020-10-02 17:28:24 -0500

    Published it to the pypi.org

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="abstcal-ycui1",
+    name="abstcal",
     version="0.5",
     author="Yong Cui",
     author_email="yong.cui01@gmail.com",
-    description="Calculate abstinence using the timeline follow back data in addiction research",
+    description="Calculate abstinence using the timeline followback data in substance research",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ycui1/abstcal",
